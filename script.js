c12b6fb0994e1c51fa7728a02baa9e0200e05d32
wumeng0224 <1169115043@qq.com>
2026-08-27 15:38:09 +0800

    添加 script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{e.preventDefault();document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'})}));
